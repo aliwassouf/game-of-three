81ac6d44ee772ff004878c87f7bf33b90cdb9054
Ali Wassouf <ali.wassouf@jimdo.com>
2021-01-04 13:08:03 +0100

    pick only one number on finding nearest

--- core/src/main/java/com/lieferando/core/functionality/FinderService.java
+++ core/src/main/java/com/lieferando/core/functionality/FinderService.java
@@ -6,13 +6,9 @@ public abstract class FinderService {
     public static int findNearestNumberToThreeOf(int number){
         int quotient = number / 3;
 
-        int firstNearestNumber = 3 * quotient;
 
-        int secondNearestNumber = (number * 3) > 0 ? (3 * (quotient + 1)) : (3 * (quotient - 1));
+        int nearestNumber = (number * 3) > 0 ? (3 * (quotient + 1)) : (3 * (quotient - 1));
 
-        if (Math.abs(number - firstNearestNumber) < Math.abs(number - secondNearestNumber))
-            return firstNearestNumber;
-
-        return secondNearestNumber;
+        return nearestNumber;
     }
 }
